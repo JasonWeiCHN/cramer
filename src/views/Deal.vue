9097d1eaa3874fa359834082efe8f55da7d87194
jasonWei <weiguangyuan@sdbattery.com>
2020-09-16 18:20:42 +0800

    no1

---
+++ src/views/Deal.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="cra-deal-wrapper">
+    <div class="cra-deal-title">
+      <div class="cra-deal-title__main">
+        <ul class="cra-deal-titleTab">
+          <li class="cra-deal-titleTab__item">About</li>
+          <li class="cra-deal-titleTab__item">Stock</li>
+          <li class="cra-deal-titleTab__item">Liquidity</li>
+          <li class="cra-deal-titleTab__item">Dividend</li>
+        </ul>
+      </div>
+    </div>
+    <div class="cra-deal-cont">
+      <div class="cra-deal-cont__main">
+        <p class="cra-deal-cont__txt">This is an experimental product, please participate with caution.</p>
+        <div class="cra-deal-cont__input">
+          <p class="cra-deal-cont__usdt">$.</p>
+          <p class="cra-deal-cont__value">3,519.<span>23</span></p>
+          <p class="cra-deal-cont__cap">Market cap</p>
+        </div>
+        <div class="cra-deal-contAddress">
+          <div class="cra-deal-contAddress__item">
+            <p class="cra-deal-contAddress__type">Contract address</p>
+            <p class="cra-deal-contAddress__txt">TsnG7y4RDDDDDDDDDDDDDDDDDDDDDDDD</p>
+          </div>
+          <div class="cra-deal-contAddress__item">
+            <p class="cra-deal-contAddress__type">Token address</p>
+            <p class="cra-deal-contAddress__txt">TsnG7y4RDDDDDDDDDDDDDDDDDDDDDDDD</p>
+          </div>
+        </div>
+        <div class="cra-deal-contBtn">Exchange now</div>
+      </div>
+      <div class="cra-deal-contData">
+        <ul class="cra-deal-contData__list">
+          <li class="cra-deal-contData__item">
+            <div class="cra-deal-contData__main">
+              <p class="cra-deal-contData__type">Last Refresh</p>
+              <p class="cra-deal-contData__mark">UTC Time</p>
+            </div>
+            <p class="cra-deal-contData__txt">2020-9-13 15:22</p>
+          </li>
+          <li class="cra-deal-contData__item">
+            <div class="cra-deal-contData__main">
+              <p class="cra-deal-contData__type">Last Refresh</p>
+              <p class="cra-deal-contData__mark">UTC Time</p>
+            </div>
+            <p class="cra-deal-contData__txt">2020-9-13 15:22</p>
+          </li>
+          <li class="cra-deal-contData__item">
+            <div class="cra-deal-contData__main">
+              <p class="cra-deal-contData__type">Last Refresh</p>
+              <p class="cra-deal-contData__mark">UTC Time</p>
+            </div>
+            <p class="cra-deal-contData__txt">2020-9-13 15:22</p>
+          </li>
+          <li class="cra-deal-contData__item">
+            <div class="cra-deal-contData__main">
+              <p class="cra-deal-contData__type">Last Refresh</p>
+              <p class="cra-deal-contData__mark">UTC Time</p>
+            </div>
+            <p class="cra-deal-contData__txt">2020-9-13 15:22</p>
+          </li>
+          <li class="cra-deal-contData__item">
+            <div class="cra-deal-contData__main">
+              <p class="cra-deal-contData__type">Last Refresh</p>
+              <p class="cra-deal-contData__mark">UTC Time</p>
+            </div>
+            <p class="cra-deal-contData__txt">2020-9-13 15:22</p>
+          </li>
+          <li class="cra-deal-contData__item">
+            <div class="cra-deal-contData__main">
+              <p class="cra-deal-contData__type">Last Refresh</p>
+              <p class="cra-deal-contData__mark">UTC Time</p>
+            </div>
+            <p class="cra-deal-contData__txt">2020-9-13 15:22</p>
+          </li>
+          <li class="cra-deal-contData__item">
+            <div class="cra-deal-contData__main">
+              <p class="cra-deal-contData__type">Last Refresh</p>
+              <p class="cra-deal-contData__mark">UTC Time</p>
+            </div>
+            <p class="cra-deal-contData__txt">2020-9-13 15:22</p>
+          </li>
+          <li class="cra-deal-contData__item">
+            <div class="cra-deal-contData__main">
+              <p class="cra-deal-contData__type">Last Refresh</p>
+              <p class="cra-deal-contData__mark">UTC Time</p>
+            </div>
+            <p class="cra-deal-contData__txt">2020-9-13 15:22</p>
+          </li>
+          <li class="cra-deal-contData__item">
+            <div class="cra-deal-contData__main">
+              <p class="cra-deal-contData__type">Last Refresh</p>
+              <p class="cra-deal-contData__mark">UTC Time</p>
+            </div>
+            <p class="cra-deal-contData__txt">2020-9-13 15:22</p>
+          </li>
+        </ul>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Deal',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="cra-index-wrapper">
+    <div class="cra-index-mark">
+      <span class="cra-index-mark__currency"></span>
+    </div>
+    <div class="cra-index-cont">
+      <div class="cra-index-cont__main">
+        <div class="cra-index-cont__logo">Logo</div>
+        <div class="cra-index-cont__txt">
+          <p>Powerful</p>
+          <p>liquidity</p>
+          <p>machine</p>
+        </div>
+        <div class="cra-index-cont__lay">
+          <p class="cra-index-cont__tit">Choosing The Right Path</p>
+          <div @click="go('/deal')" class="cra-index-cont__arrow"></div>
+        </div>
+
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  },
+  methods:{
+    go(path){
+      this.$router.push({
+        path
+      })
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+
+</style>
